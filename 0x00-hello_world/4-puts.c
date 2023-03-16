44f486494ba590c4f4af6fbe0b15796182c20688
AlexNduta <wasremoved@gmail.com>
2023-03-16 22:40:48 +0300

    wueshs after comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,16 @@
 /**
  * this is a function that prints text
  */
+/** 
+ * i keep typing
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
-
+/**
+ * this returns a a zero integer
+ */
 	return (0);
 }
