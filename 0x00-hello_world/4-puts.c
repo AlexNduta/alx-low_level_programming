570cbfb2fd8f6174f4e5f350493bf9a28d7ed3f2
AlexNduta <wasremoved@gmail.com>
2023-03-16 22:19:35 +0300

    wuesh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,8 @@
  */
 int main(void)
 {
-/* 
- * this is a string of characters defined
- */
 	char str[] = "\"Programming is like building a multilingual puzzle";
-/* 
- * we print the contents of the str array by passsing
- * the variable to the puts()
- */
+
 	puts(str);
 
 	return (0);
