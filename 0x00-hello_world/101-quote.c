930940e1386e632d82dce3c556b699e85043bc40
AlexNduta <wasremoved@gmail.com>
2023-03-17 05:27:43 +0300

    sizes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * A function that prints to the standard input
+ * Return : 1 (Success)
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg) - 1);
+	return (1);
+}
