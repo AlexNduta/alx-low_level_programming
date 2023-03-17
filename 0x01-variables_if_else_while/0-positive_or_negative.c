1ef44ec7bfd7e5e589dde2da3eba9be7d59c3b74
AlexNduta <wasremoved@gmail.com>
2023-03-17 22:37:28 +0300

    001

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
   *main - generates random numbers and checks if its positive
   *@n : this is the variable that holds the vallue of theb= random function
-  *Return : 0 is always(Sucess)
+  *Return : 0 always (Success)
 */
 
 int main(void)
