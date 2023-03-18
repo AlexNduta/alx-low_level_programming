783bd88cc3be6158820862fc3b2167d26b0edb78
AlexNduta <wasremoved@gmail.com>
2023-03-18 05:50:22 +0300

    11.4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <time.h>
 /**
  *main - generates random numbers and checks if its positive
- *@n : this is the variable that holds the vallue of theb= random function
- *Return : 0 always (Success)
+ *
+ *Return: always 0
  */
 
 int main(void)
@@ -23,5 +23,6 @@ if (n > 0)
 {
 	printf("%d is negative\n", n);
 }
+	printf("\n");
 	return (0);
 }
