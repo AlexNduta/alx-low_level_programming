01d658e5b2f49b863b858514d91c0230e419a1b7
AlexNduta <wasremoved@gmail.com>
2023-03-19 13:49:20 +0300

    printing out a combo of the possible numbers without duplicates

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0 ; x < 8; x++)
+	{
+		for (y = x + 1 ; y < 9; y++)
+		{
+			for (z = y + 1; z < 10; z++)
+			{
+				putchar((x % 10) + '0');
+				putchar((y % 10) + '0');
+				putchar((z % 10) + '0');
+				if (x != 8 || y != 9 || z != 10)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
