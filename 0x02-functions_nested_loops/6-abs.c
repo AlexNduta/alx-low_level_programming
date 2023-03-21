99358893b9e0dde0a662a16d90b4a53c55fe9d82
AlexNduta <wasremoved@gmail.com>
2023-03-22 02:59:28 +0300

    custom headerfile: printing absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <inttypes.h>
+#include 'main.h"
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int)
+{
+	abs();
+	returns (0);
+}
