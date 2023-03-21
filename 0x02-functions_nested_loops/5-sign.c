8a6d9e6d7baab237a16fc8f96df13c3bac40c363
AlexNduta <wasremoved@gmail.com>
2023-03-22 02:38:35 +0300

    custom headerfile: printing number characters:correction2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  *print_sign checks fo use cases and prints characters
+ *@n: the variable that determines everything
  *Return: 1 if the value is greater,
  *				0 if its zero ,
  *				-if the number is less than 0.
