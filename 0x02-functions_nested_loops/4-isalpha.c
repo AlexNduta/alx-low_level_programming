cc1fee37ad1326c1383ba2b1be80b5ef0903ab02
AlexNduta <wasremoved@gmail.com>
2023-03-21 22:01:42 +0300

    custom header files:checks for alphabet letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: variable that is used to make checks
+ *Return: 1 if its alphabet, 0 otherwise
+ */
+in _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
