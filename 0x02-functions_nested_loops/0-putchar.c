5b781f3d44c239753cf286f952adf02d5b28a357
AlexNduta <wasremoved@gmail.com>
2023-03-21 19:41:29 +0300

    my first custom header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main -prints chracters to the  stdout
+ *Return: 0 is always (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
