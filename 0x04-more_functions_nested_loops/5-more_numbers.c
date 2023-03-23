ddd38eea0596e4d2b02ad5d8b1fcc6ad0787f1a5
AlexNduta <wasremoved@gmail.com>
2023-03-23 21:34:13 +0300

    printing out in bulk

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 1 to 14 10 times
+ *Returns: void
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+		for (x = 1; x <= 10; x++)
+		{
+			for (y = 0; y < 15; y++)
+			{
+				if (y >= 10)
+					_putchar('1');
+				_putchar((y % 10) + '0');
+			}
+		}
+	_putchar('\n');
+}
+
