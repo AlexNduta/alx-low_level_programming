5646ba61dc9d5bf6f043d33eb2b52b0f02edb59b
AlexNduta <wasremoved@gmail.com>
2023-03-23 14:01:33 +0300

    check fo upper case letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks if the provided letter is uppercase
+ *Return: 1 in case of sucess
+ *Return: 0 incase of failure
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+else
+		return (0);
+}
