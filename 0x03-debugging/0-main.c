b35011c72540c121f9bd2d03093ea16daf4b2ca1
AlexNduta <wasremoved@gmail.com>
2023-03-24 03:35:15 +0300

    error driven development:v4

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,8 @@
+
 #include "main.h"
+
 /**
-* main - tests function for positive or negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
@@ -10,5 +12,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
