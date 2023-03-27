97a68210e53c7f3c73a1fc4b24a5ecb7613a1560
AlexNduta <wasremoved@gmail.com>
2023-03-27 22:25:20 +0300

    pointers and more pointers: number 0 and 1, betty edit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  *reset_to_98 - holds the value 98 in the memory
+ *@n: the variable in question
  *Returns: void
  */
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  *swap_int - used to swap variables
+ *@a and @b: variable pointers in question
  *Returns: void
  */
 void swap_int(int *a, int *b)
