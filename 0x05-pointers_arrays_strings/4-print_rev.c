adaeb6622e2738a96580f2a706cf1538e6ce414e
AlexNduta <wasremoved@gmail.com>
2023-03-28 04:01:24 +0300

    pointers and more pointers: no 4, reversing my string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_rev - prints a string in reverse
+ *@s: the variable pointer in question
+ *Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
