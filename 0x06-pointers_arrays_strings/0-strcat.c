43147cb19f4aef2c2d0a0042250d3cb60b858958
AlexNduta <wasremoved@gmail.com>
2023-03-30 14:25:16 +0300

    pointers on pointer: strcat and strncat: edit1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_strcat - the concatinates our source string to our destination string
- *@*dest: pointer to the first element of our destination array
+ *@dest: pointer to the first element of our destination array
  *@src: pointer to the first element of our source  array
  *
  *Return: the destination pointer
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,17 @@
-#include "main.h"
-/**
- *_strncat - concatenates string arrays
- *@dest: thisis the starting string
- *@src: this is the string to be joined
- *
- *Return: dest
- */
-char* _strncat(char *dest, char *src, int n)
-{
-	int dest_len = strlen(dest);
-	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len] = '\0';
-return (dest);
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return(dest);
 }
