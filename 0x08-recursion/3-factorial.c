c36e3c00795814567a19c08ed55d9a1cc4974b5c
AlexNduta <wasremoved@gmail.com>
2023-04-06 04:21:59 +0300

    Reursion: factorial calculations

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *factorial - calculates the factorial of a given number
+ *@n: the input number
+ *
+ *Return: the value of the current factorial
+ */
+int factorial(int n)
+{
+	if (n <= 1)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
