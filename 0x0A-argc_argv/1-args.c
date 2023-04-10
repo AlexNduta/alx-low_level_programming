d870ab7be6d095b8d4f748f4eacf5c68ae327725
AlexNduta <wasremoved@gmail.com>
2023-04-10 09:01:50 +0300

    argc and argv: printing out the numnber of comandline arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: 0 is always success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
