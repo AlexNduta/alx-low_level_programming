06bc7167d64733a02ff1e3c7d0bf7f5974cc454e
AlexNduta <wasremoved@gmail.com>
2023-04-10 08:53:18 +0300

    argc and argv: printing out the first argument:correction101

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
  *main - main entry function
  *@argc: counter argument variable
  *@argv: argunemt vector
- *Return: 0 is always sucess
+ *
+*Return: 0 is always sucess
  */
 int main(int argc,char* argv[])
 {
 	int i = 0;
- (void) argc;
+	(void) argc;
 	printf("%s\n", argv[i]);
 	return (0);
 }
