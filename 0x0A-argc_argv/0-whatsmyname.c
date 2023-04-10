5ee7cd0f7be41bc1f67181023171b84f24a6f968
AlexNduta <wasremoved@gmail.com>
2023-04-10 08:45:42 +0300

    argc and argv: printing out the first argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - main entry function
+ *@argc: counter argument variable
+ *@argv: argunemt vector
+ */
+int main(int argc,char* argv[])
+{
+	int i = 0;
+ (void) argc;
+	printf("%s", argv[i]);
+	return (0);
+}
