bc85caaa8498cbad50ca3f2d702d20935825deb1
AlexNduta <wasremoved@gmail.com>
2023-04-12 22:18:52 +0300

    malloc: A function to free up memory allocated

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *free_grid - free previously allocated mem'
+ *@grid: pointer to a 2D array
+ *@heigt: the number of rows
+ *
+ *Return: Nutting
+ */
+void free_grid(int **grid, int height)
+{
+	int q;
+
+	while (q < height)
+		free(grid[q]);
+		i++;
+	free(grid)
+}
