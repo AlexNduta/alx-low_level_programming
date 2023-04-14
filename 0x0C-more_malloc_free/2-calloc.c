a1aba7a50920d01dd8da9ed9490687ac87ba8a2a
AlexNduta <wasremoved@gmail.com>
2023-04-14 03:50:29 +0300

    more malloc and free:custom calloc function: error 106

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	pointer = malloc(sizeof(nmemb) * size);
+	pointer = malloc(nmemb * size);
 
 	if (pointer  == NULL)
 		return (NULL);
