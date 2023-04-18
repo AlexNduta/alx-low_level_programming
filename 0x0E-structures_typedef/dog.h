175698d3e0f260dccacde32d34416cfb0aee5cba
AlexNduta <wasremoved@gmail.com>
2023-04-18 03:10:40 +0300

    structures and typedef: number 0 and 1:correction 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,17 +7,12 @@
  *@owner: who is the guardian to the dog
  *
  */
-struct{
+struct
+{
 	char *name;
 	float *age;
 	char  *owner;
 };
-#include<stdio.h>
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
 #endif
