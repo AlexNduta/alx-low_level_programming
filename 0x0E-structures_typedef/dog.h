d8b84c7224290897918675ab666edc5931c6b619
AlexNduta <wasremoved@gmail.com>
2023-04-18 04:55:20 +0300

    structures and typedef: project 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,11 @@ struct dog
 };
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
