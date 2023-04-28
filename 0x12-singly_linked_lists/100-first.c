9c58eb7cdb76b64064c8d5202fedd7a951116606
AlexNduta <wasremoved@gmail.com>
2023-04-28 03:50:27 +0300

    advanced tasks:correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
  */
 void mystartupFun(void) __attribute__((constructor));
 
-void mystartupfun(void)
+void mystartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
