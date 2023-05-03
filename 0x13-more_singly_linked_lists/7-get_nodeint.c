0211bad7c1db12dad64e46487bf1122179fcd99a
AlexNduta <wasremoved@gmail.com>
2023-05-03 04:28:17 +0300

    more linked_list: nth node of our list:correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
  *
  *Return:pointer to the nth node
  */
-listint_t *get_nodeint_at_index(list_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	size_t i;
 
