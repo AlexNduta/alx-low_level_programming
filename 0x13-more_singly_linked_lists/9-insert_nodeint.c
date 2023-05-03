badf4453c6a6059bd04389faf0699bb06dec2347
AlexNduta <wasremoved@gmail.com>
2023-05-03 05:08:10 +0300

    more linked_list:insert a node at given posirion: correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,19 +7,27 @@
  *
  *Return: address of the new node
  */
-listint_t *insert_nodeint_at_index(lisint_t **head, size_t idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, size_t idx, int n)
 {
 	size_t i;
 	listint_t *temp, *new;
 
 	if (head == NULL)
 		return (NULL);
+	if (idx != 0)
+	{
+		temp = *head;
+		for (i = 0; i < idx -1 && temp != NULL; i++)
+			temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
 
-	new = malloc(sizeof(listint_t))
+	new = malloc(sizeof(listint_t));
 		if (new == NULL)
 			return (NULL);
 
-	new->n n;
+	new->n = n;
 	if (idx != 0)
 	{
 		new->next = *head;
