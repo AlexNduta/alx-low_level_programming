1fc71580a2d55a52955fe7210d127a4d9653a064
AlexNduta <wasremoved@gmail.com>
2023-05-04 20:31:47 +0300

    Bit manipulation: 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,25 @@
 #include "main.h"
 /**
  *binary_to_unint - convert an input astring to int
- *@b: input string
+ *@b: points to a sttring 0 & 1
  *
- *Return: the result
+ *Return: converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
 	size_t output = 0;
-	const char *ptr =b;
+	int i = 0;
 	/* chek if input string is null */
 	if (b == NULL)
 		return (0);
  /*initialise a variable to carry the result*/
 
-	while (*ptr)
+	while (b[i] != '\0')
 	{
-		if (*ptr == '0' || *ptr == '1')
-		{
-			output = (output << 1) + (*ptr - '0');
-			ptr++;
-		} else
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
+		output = output * 2 +(b[i] - '0');
+	i++;
 	}
 	return (output);
 }
