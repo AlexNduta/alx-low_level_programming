6ac44afa0c83cddc7797533b4bcc92b1656e7542
AlexNduta <wasremoved@gmail.com>
2023-05-08 21:54:18 +0300

    file I/O:1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,7 @@ int create_file(const char *filename, char *text_content)
 	{
 	/*calculate then length of the  text content */
 		while (text_content[length] != '\0')
-		{
 			length++;
-		}
 	}
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
@@ -27,6 +25,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1 || nwrite == -1)
 		return (-1);
 	close(fd);
-	return (-1);
+	return (1);
 }
 
