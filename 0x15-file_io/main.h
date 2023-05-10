7c380fdcaa7fb04e64cf81827a5005bd00b09efc
AlexNduta <wasremoved@gmail.com>
2023-05-10 04:17:48 +0300

    file I/O:2

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,4 @@
-#ifndef fILE_H
+#ifndef FILE_H
 #define FILE_H
 
 #include <stdio.h>
@@ -9,7 +9,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-
-
 #endif
