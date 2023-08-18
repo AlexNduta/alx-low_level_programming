fb18dbc293a2c87f5c32f0f39622a8d0b6b19f4f
Alex Nduta <wasremoved@gmail.com>
2023-08-18 04:59:50 +0300

    adding an item on the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,36 +1,36 @@
 #include "list.h"
 
 /**
- *dlistint_t - adds an element to the end of our list
- *@head: pointer to the head of our list
- *@n: value to add at our list
- *Return: pointer to our new node
- */
+*dlistint_t - adds an element to the end of our list
+*@head: pointer to the head of our list
+*@n: value to add at our list
+*Return: pointer to our new node
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    if (head == NULL)
-        return (NULL);
-    dlistint_t current;
-    dlistint_t newItem = GetNewNode(n);
-    /*if memory allocation is NULL*/
-    if newItem == NULL
-        return (NULL);
+if (head == NULL)
+return (NULL);
+dlistint_t current;
+dlistint_t newItem = GetNewNode(n);
+/*if memory allocation is NULL*/
+if newItem == NULL
+return (NULL);
 /*when the list is empty */
-    if (*head == NULL)
-    {
-        newItem->prev = NULL;
-        *head = new'
-        return (newitem);
-    }
-    /* as long as we have not reached the last item, keep moving forward */
-   current = *head;
-    while(current->next != NULL)
-    {
-        current = current-next;
-    }
+if (*head == NULL)
+{
+newItem->prev = NULL;
+*head = new'
+return (newitem);
+}
+/* as long as we have not reached the last item, keep moving forward */
+current = *head;
+while(current->next != NULL)
+{
+current = current-next;
+}
 /*when you get to the last item, make the current node point to the new item*/
-    current->next = newItem;
-    newItem->prev =current;
+current->next = newItem;
+newItem->prev =current;
 
 return (newItem);
 }
