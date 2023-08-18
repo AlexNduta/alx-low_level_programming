7efb6371e83b892ad9c859e79ba51546c45a43d1
Alex Nduta <wasremoved@gmail.com>
2023-08-18 05:29:07 +0300

    fixinf issues

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 unsigned int i;
 
 if (head == NULL)
-return (NULL)
+return (NULL);
 if (index == 0)
 return (head);
 for (i = 0; i < index; i++)
