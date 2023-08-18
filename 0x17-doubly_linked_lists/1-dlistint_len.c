072ec7f6063e884e2c0e57e8a2b4cb7a1ecbacb5
Alex Nduta <wasremoved@gmail.com>
2023-08-18 03:10:57 +0300

    counting number of lements in a listw

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- *dlistint_len - returns the number of elements in a list
- *@h: pointer to the head node
- *Return: number of elements in a node
- */
+*dlistint_len - returns the number of elements in a list
+*@h: pointer to the head node
+*Return: number of elements in a node
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *temp = h;
-    size_t counter = 0;
+const dlistint_t *temp = h;
+size_t counter = 0;
 
-    while (temp != NULL)
-    {
-        temp = temp->next;
-        counter++;
-    }
+while (temp != NULL)
+{
+temp = temp->next;
+counter++;
+}
 return (counter);
 }
