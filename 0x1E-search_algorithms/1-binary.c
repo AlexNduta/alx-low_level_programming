d1488f4fc6e5a0e1a89e044a07a6af0c8c560658
Alex Nduta <wasremoved@gmail.com>
2023-12-13 21:54:59 +0300

    search algorith

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,11 +1,10 @@
 #include "search_algos.h"
 /**
  *binary_search - search for target element by dividing by half
- *
  *@array: pointer to the first element of the array we are traversing
  *@size: the size of our array
  *@value: The target value to look for
- *Return the target value on success
+ *Return: the target value on success
  **/
 int binary_search(int *array, size_t size, int value)
 {
