4f039e283a04de94351bd635f422b80928a6e2c5
Alex Nduta <wasremoved@gmail.com>
2023-12-13 19:10:32 +0300

    search algorith

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 size_t i = 0;
 
 if (!array)
-    return (-1);
+return (-1);
 for (i = 0; i < size; i++)
 {
 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
