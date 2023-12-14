594a965b41d9a30742d07d00bbb6055b387ff5df
Alex Nduta <wasremoved@gmail.com>
2023-12-14 21:29:44 +0300

    search algorith

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,62 +1,51 @@
 #include "search_algos.h"
-/**
-*binary_search - search for target element by dividing by half
-*@array: pointer to the first element of the array we are traversing
-*@size: the size of our array
-*@value: The target value to look for
-*Return: the target value on success
-**/
 
+/**
+ * print_arr - helper func to print array everytime array is halved
+ * @array: array
+ * @l: left index of original array
+ * @r: right index of original array
+ */
+void print_arr(int *array, size_t l, size_t r)
+{
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		if (i != r)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+}
+
+/**
+ * binary_search - search mids
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
 int binary_search(int *array, size_t size, int value)
 {
-int first_num = 0;
-int last_num = size - 1;
-int mid_num = 0;
+	size_t mid = 0;
+	size_t l = 0;
+	size_t r = size - 1;
 
-if (!array)
-return (-1);
+	if (!array)
+		return (-1);
 
-while (first_num <= last_num)
-{
-/*print_array(array, size); */
-print_array(array + first_num, last_num - first_num + 1);
-
-mid_num = (last_num + first_num) / 2;
-if (array[mid_num] == value)
-{
-return (mid_num);
+	while (l <= r)
+	{
+		print_arr(array, l, r);
+		mid = (l + r) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return (-1);
 }
-else if (array[mid_num] > value)
-{
-last_num = mid_num - 1;
-}
-else
-{
-first_num = mid_num + 1;
-}
-
-}
-return (0);
-}
-
-
-/**
-*print_array - prints the array
-*@array: pointer to the first element of our array
-*@size: size of the array
-**/
-
-void print_array(int *array, size_t size)
-{
-size_t i;
-printf("Searched in: ");
-for (i = 0; i < size; i++)
-{
-printf("%d ", array[i]);
-if (i < size - 1)
-printf(",");
-}
-printf("\n");
-}
-
-
