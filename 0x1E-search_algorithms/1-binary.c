928e6f1222bbbcf7c1acd782cd866fa20f93d180
Alex Nduta <wasremoved@gmail.com>
2023-12-14 21:20:31 +0300

    search algorith

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,29 +1,27 @@
 #include "search_algos.h"
 /**
- *binary_search - search for target element by dividing by half
- *@array: pointer to the first element of the array we are traversing
- *@size: the size of our array
- *@value: The target value to look for
- *Return: the target value on success
- **/
+*binary_search - search for target element by dividing by half
+*@array: pointer to the first element of the array we are traversing
+*@size: the size of our array
+*@value: The target value to look for
+*Return: the target value on success
+**/
+
 int binary_search(int *array, size_t size, int value)
 {
 int first_num = 0;
 int last_num = size - 1;
-size_t i = 0;
 int mid_num = 0;
 
 while (first_num <= last_num)
 {
-for (i = 0; i < size; i++)
-{
-printf("searching in array: %d\n", *array);
-}
+/*print_array(array, size); */
+print_array(array + first_num, last_num - first_num + 1);
 
-mid_num = (first_num + last_num) / 2;
-if (array[mid_num] < value)
+mid_num = (last_num + first_num) / 2;
+if (array[mid_num] == value)
 {
-first_num = mid_num + 1;
+return (mid_num);
 }
 else if (array[mid_num] > value)
 {
@@ -31,26 +29,29 @@ last_num = mid_num - 1;
 }
 else
 {
-return (mid_num);
+first_num = mid_num + 1;
 }
-return (-1);
-print_array(array, size);
+
 }
 return (0);
 }
 
+
 /**
- *print_array - prints the array
- *@array: pointer to the first element of our array
- *@size: size of the array
- **/
+*print_array - prints the array
+*@array: pointer to the first element of our array
+*@size: size of the array
+**/
 
 void print_array(int *array, size_t size)
 {
 size_t i;
+printf("Searched in:");
 for (i = 0; i < size; i++)
 {
-printf("searching in array: %d\n", array[i]);
+printf("%d ", array[i]);
+if (i < size - 1)
+printf(",");
 }
 printf("\n");
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,3 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,3 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int main(void);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t size);
+int binary_search(int *array, size_t size, int value);
+
+#endif
